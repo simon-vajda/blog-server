2a338b41230fecbe4cb3e578f9f8c95e5ef4436f
Simon Vajda <simon.vajda23@gmail.com>
2022-03-20 15:48:47 +0100

    AppUser name column instead of firstName + lastName

--- src/main/java/hu/vsimon/blogserver/AppUser.java
+++ src/main/java/hu/vsimon/blogserver/AppUser.java
@@ -24,11 +24,8 @@ public class AppUser {
     @Column(name = "password", nullable = false, columnDefinition = "TEXT")
     private String password;
 
-    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
-    private String firstName;
-
-    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
-    private String lastName;
+    @Column(name = "name", nullable = false, columnDefinition = "TEXT")
+    private String name;
 
     @Column(name = "posts")
     @OneToMany(mappedBy = "user")
